1e187e967ddba5ac02ed988e1e132f82a66bc4dc
RJ Dellecese <rjdellecese@gmail.com>
2015-11-17 00:24:55 +0000

    description and address must be present, length of name must be 3 chars min

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,5 +2,6 @@ class Place < ActiveRecord::Base
   self.per_page = 2
 
   belongs_to :user
-  validates :name, :presence => true
+  validates :name, :description, :address, :presence => true
+  validates :name, length: { minimum: 3 }
 end
