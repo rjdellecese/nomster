ed15cfd156de653670c272b31b4784e724545db0
RJ Dellecese <rjdellecese@gmail.com>
2015-11-22 19:40:16 +0000

    placed photo upload form in modal, styled photos on show page, added last photo of each place on index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def conditional_wrapper(condition=true, options={}, &block)
+    options[:tag] ||= :div
+    if condition == true
+      concat content_tag(options[:tag], capture(&block), options.delete_if{|k,v| k == :tag})
+    else
+      concat capture(&block)
+    end
+  end  
 end
